64c53f6c2590f3ce6acc67d4acb318552457594a
hughlau <hughlaunet@gmail.com>
2018-07-27 16:18:22 +0800

    version 3.0

--- new/statics/js/custom.js
+++ new/statics/js/custom.js
@@ -1,21 +1,39 @@
 
+function GetUrlRelativePath()
+{
+	var url = document.location.toString();
+	var arrUrl = url.split("//");
 
+	var start = arrUrl[1].indexOf("/");
+	var relUrl = arrUrl[1].substring(start);//stop省略，截取从start开始到结尾的所有字符
+
+	if(relUrl.indexOf("?") != -1){
+		relUrl = relUrl.split("?")[0];
+	}
+	return relUrl;
+}
 jQuery(document).ready(function($){
-		$(window).scroll(function() {
-			if ($(this).scrollTop() > 200) {
-				$('.go-top').fadeIn(200);
-					} else {
-						$('.go-top').fadeOut(200);
-					}
-				});		
-				// Animate the scroll to top
-				$('.go-top').click(function(event) {
-					event.preventDefault();
-				
-					$('html, body').animate({scrollTop: 0}, 300);
-				});
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 200) {
+			$('.go-top').fadeIn(200);
+		} else {
+			$('.go-top').fadeOut(200);
+		}
+	});		
+	// Animate the scroll to top
+	$('.go-top').click(function(event) {
+		event.preventDefault();
 	
+		$('html, body').animate({scrollTop: 0}, 300);
+	});
 
+	$('.navbar-right').find('li').each(function(){
+		var curhref=$(this).children('a')[0].getAttribute('href');
+		var curUrl=GetUrlRelativePath();
+		if(curhref!="/" && curUrl.indexOf(curhref)>-1){
+			$(this).addClass('currentli');
+		}
+	});
 });
 
 jQuery.noConflict()(function ($) {
